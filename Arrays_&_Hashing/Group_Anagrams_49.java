2db59c065676fa71fdc83e576dc2eb603a865c27
Justin Sherrell <Superjustin44@gmail.com>
2023-10-26 02:32:57 -0700

    up

---
+++ Arrays_&_Hashing/Group_Anagrams_49.java
@@ -0,0 +1,7 @@
+import java.util.List;
+
+public class Group_Anagrams_49 {
+    public static List<List<String>> groupAnagrams(String[] strs) {
+        return null;
+    }
+}
--- Arrays_&_Hashing/practice.java
+++ Arrays_&_Hashing/practice.java
@@ -3,28 +3,18 @@ import java.util.HashSet;
 
 public class practice {
 
-    public static boolean isAnagram(String s, String t) {
-        if (s.length() != t.length())
-            return false;
+    public static int[] twoSum(int[] nums, int target) {
+        HashMap<Integer, Integer> map = new HashMap<>();
 
-        HashMap<Character, Integer> map = new HashMap<>();
+        for(int i = 0; i < nums.length; i++) {
+            int num = nums[i];
+            int diff = target - num;
 
-        for (char c : s.toCharArray()) {
-            int count = map.getOrDefault(c, 0);
-            map.put(c, count + 1);
+            if (map.containsKey(diff))
+                return new int[] {map.get(diff), i};
+
+            map.put(num, i);
         }
-
-        for (char c : t.toCharArray()) {
-            if (!map.containsKey(c))
-                return false;
-
-            int count = map.get(c);
-            if (count == 1)
-                map.remove(c);
-            else
-                map.put(c, count - 1);
-        }
-
-        return map.isEmpty();
+        return null;
     }
 }
