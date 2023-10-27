70c118c905bce9b671bd1f9299c199306694bacc
Justin Sherrell <Superjustin44@gmail.com>
2023-10-26 23:51:45 -0700

    up

--- Arrays_&_Hashing/Group_Anagrams_49.java
+++ Arrays_&_Hashing/Group_Anagrams_49.java
@@ -1,7 +1,34 @@
-import java.util.List;
+import java.util.*;
 
 public class Group_Anagrams_49 {
     public static List<List<String>> groupAnagrams(String[] strs) {
-        return null;
+        HashMap<String, List<String>> map = new HashMap<>();
+
+        for (String word : strs) {
+            char[] chars = word.toCharArray();
+
+            Arrays.sort(chars);
+            String sortedWord = new String(chars);
+
+            if (!map.containsKey(sortedWord))
+                map.put(sortedWord, new ArrayList<>());
+
+            map.get(sortedWord).add(word);
+        }
+
+        return new ArrayList<>(map.values());
+    }
+
+
+
+    public static void main(String[] args) {
+
+
+        String[] strs = {"eat", "tea", "tan", "ate", "nat", "bat"};
+
+
+        System.out.println(groupAnagrams(strs));
+
+
     }
 }
