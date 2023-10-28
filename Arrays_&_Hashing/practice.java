88ff99d8f7b7cf70dedaa245ed3d694863decee8
Justin Sherrell <Superjustin44@gmail.com>
2023-10-28 00:43:35 -0700

    practice HashSet Contains Duplicate

--- Arrays_&_Hashing/practice.java
+++ Arrays_&_Hashing/practice.java
@@ -1,6 +1,14 @@
-import java.util.*;
+import java.util.HashSet;
 
 public class practice {
 
-
+    public boolean containsDuplicate(int[] nums) {
+        HashSet<Integer> uniques = new HashSet<>();
+        for(int i : nums) {
+            if (uniques.contains(i))
+                return true;
+            uniques.add(i);
+        }
+        return false;
+    }
 }
