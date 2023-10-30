5c0b19462f18681eafab014354514d8bf3646844
Justin Sherrell <Superjustin44@gmail.com>
2023-10-30 00:15:42 -0700

    topKFrequent

--- Arrays_&_Hashing/Frequent_Elements_347.java
+++ Arrays_&_Hashing/Frequent_Elements_347.java
@@ -1,40 +1,82 @@
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 // Last Practiced: Oct 29th 2023
 
 public class Frequent_Elements_347 {
-    class Solution3 {
-        /**
-         * Time Complexity: O(n)
-         * Space Complexity: O(n)
-         */
-        public int[] topKFrequent(int[] nums, int k) {
-            Map<Integer, Integer> count = new HashMap<>();
-            List<Integer>[] bucket = new ArrayList[nums.length + 1];
 
-            for (int num : nums)
-                count.merge(num, 1, Integer::sum);
+    /**
+     * Time Complexity: O(n)
+     * Space Complexity: O(n)
+     */
+    public static int[] topKFrequent(int[] nums, int k) {
+        Map<Integer, Integer> count = new HashMap<>();
+        List<Integer>[] bucket = new ArrayList[nums.length + 1];
 
-            for (int key : count.keySet()) {
-                int freq = count.get(key);
-                if (bucket[freq] == null)
-                    bucket[freq] = new ArrayList<>();
-                bucket[freq].add(key);
-            }
+        for (int num : nums)
+            count.merge(num, 1, Integer::sum);
 
-            int index = 0;
-            int[] res = new int[k];
-            for (int i = nums.length; i >= 0; i--)
-                if (bucket[i] != null)
-                    for (int val : bucket[i]) {
-                        res[index++] = val;
-                        if (index == k)
-                            return res;
-                    }
-            return res;
+        for (int key : count.keySet()) {
+            int freq = count.get(key);
+            if (bucket[freq] == null)
+                bucket[freq] = new ArrayList<>();
+            bucket[freq].add(key);
         }
+
+        System.out.println(Arrays.toString(bucket));
+
+        int index = 0;
+        int[] res = new int[k];
+        for (int i = nums.length; i >= 0; i--)
+            if (bucket[i] != null)
+                for (int val : bucket[i]) {
+                    res[index++] = val;
+                    if (index == k)
+                        return res;
+                }
+
+        return res;
+    }
+
+    public static void main(String[] args) {
+        // Test Case 1
+//        int[] nums1 = {1, 1, 1, 2, 2, 3};
+//        int k1 = 2;
+//        int[] result1 = topKFrequent(nums1, k1);
+//        System.out.println(Arrays.toString(result1)); // Expected output: [1, 2]
+
+        // Test Case 2
+        int[] nums2 = {1, 1, 1, 2, 2, 3, 3, 3, 3};
+        int k2 = 2;
+        int[] result2 = topKFrequent(nums2, k2);
+        System.out.println(Arrays.toString(result2)); // Expected output: [3, 1]
+//
+//        // Test Case 3
+//        int[] nums3 = {4, 1, -1, 2, -1, 2, 3};
+//        int k3 = 2;
+//        int[] result3 = topKFrequent(nums3, k3);
+//        System.out.println(Arrays.toString(result3)); // Expected output: [-1, 2]
+
+        // Add more test cases as needed to cover various scenarios.
+
+
+//        int[] nums = {1, 1, 2, 2, 3, 3, 3, 4, 4, 4, 4, 4};
+//        Map<Integer, Integer> count = new HashMap<>();
+//
+//
+////        Integer::sum is a method reference in Java.It's used to refer to a method in the Integer class. In this case, it refers to the static
+////        method sum in the Integer class, which is used to add two integers together.
+//
+//        for (int num : nums) {
+//            // Using count.merge to update the count of each element
+//            count.merge(num, 1, Integer::sum);
+//        }
+//
+//        // Print the frequency of each element
+//        for (Map.Entry<Integer, Integer> entry : count.entrySet()) {
+//            int num = entry.getKey();
+//            int frequency = entry.getValue();
+//            System.out.println("Element " + num + " appears " + frequency + " times.");
+//        }
+
     }
 }
--- Arrays_&_Hashing/Valid_Anagram_242.java
+++ Arrays_&_Hashing/Valid_Anagram_242.java
@@ -1,49 +1,47 @@
 import java.util.Arrays;
 import java.util.HashMap;
+import java.util.Map;
 
 // Last Practiced: Oct 29th 2023
 
 public class Valid_Anagram_242 {
 
-//    public static boolean isAnagram(String s, String t) {
-//        if (s.length() != t.length())
-//            return false;
-//
-//        HashMap<Character, Integer> map = new HashMap<>();
-//
-//        for (char c : s.toCharArray()) {
-//            int count = map.getOrDefault(c, 0);
-//            map.put(c, count + 1);
-//        }
-//
-//        for (char c : t.toCharArray()) {
-//            if (!map.containsKey(c))
-//                return false;
-//
-//            int count = map.get(c);
-//            if (count == 1)
-//                map.remove(c);
-//            else
-//                map.put(c, count - 1);
-//        }
-//
-//        return map.isEmpty();
-//    }
-
     public static boolean isAnagram(String s, String t) {
-        if (s.length() != t.length()) {
+        if (s.length() != t.length())
             return false;
+
+        Map<Character, Integer> charCount = new HashMap<>();
+
+        for (char c : s.toCharArray())
+            charCount.merge(c, 1, Integer::sum);
+
+
+        for (char c : t.toCharArray())
+            charCount.merge(c, -1, Integer::sum);
+
+
+        for (int count : charCount.values()) {
+            if (count != 0)
+                return false;
         }
 
-        char[] sArray = s.toCharArray();
-        char[] tArray = t.toCharArray();
-
-        Arrays.sort(sArray);
-        Arrays.sort(tArray);
-
-        // Convert sorted character arrays back to strings and compare them.
-        return Arrays.equals(sArray, tArray);
+        return true;
     }
+
+//    public static boolean isAnagram(String s, String t) {
+//        if (s.length() != t.length()) {
+//            return false;
+//        }
+//
+//        char[] sArray = s.toCharArray();
+//        char[] tArray = t.toCharArray();
+//
+//        Arrays.sort(sArray);
+//        Arrays.sort(tArray);
+//
+//        // Convert sorted character arrays back to strings and compare them.
+//        return Arrays.equals(sArray, tArray);
+//    }
 
     public static void main(String[] args) {
         // Test cases
