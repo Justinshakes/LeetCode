788e63beae23c4b05921e9d0491e9efa37245a76
Justin Sherrell <Superjustin44@gmail.com>
2023-11-06 02:40:16 -0800

    Update practice.java

--- Arrays_&_Hashing/practice.java
+++ Arrays_&_Hashing/practice.java
@@ -2,16 +2,7 @@ import java.util.*;
 
 public class practice {
 
-    public static boolean isAnagram(String s, String t) {
-        if (s.length() != t.length())
-            return false;
-
-        char[] sArray = s.toCharArray();
-        char[] tArray = t.toCharArray();
-
-        Arrays.sort(sArray);
-        Arrays.sort(tArray);
-
-        return Arrays.equals(sArray, tArray);
+    public static List<List<String>> groupAnagrams(String[] strs) {
+        
     }
 }
