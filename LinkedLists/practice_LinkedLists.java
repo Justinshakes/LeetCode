9d3d161f5d619d3dcbefa9a325c2c0e0753de1c4
Justin Sherrell <Superjustin44@gmail.com>
2023-11-07 05:53:20 -0800

    Update practice_LinkedLists.java

--- LinkedLists/practice_LinkedLists.java
+++ LinkedLists/practice_LinkedLists.java
@@ -18,8 +18,25 @@ public class practice_LinkedLists {
     }
 
     public static ListNode mergeTwoLists(ListNode l1, ListNode l2) {
+        ListNode dummyHead = new ListNode(0);
+        ListNode current = dummyHead;
 
+        while (l1 != null && l2 != null) {
+            if (l1.val < l2.val) {
+                current.next = l1;
+                l1 = l1.next;
+            } else {
+                current.next = l2;
+                l2 = l2.next;
+            }
+            current = current.next;
+        }
 
-        return null;
+        if (l1 == null)
+            current.next = l2;
+        else
+            current.next = l1;
+
+        return dummyHead.next;
     }
 }
