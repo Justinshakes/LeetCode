195ab3e23fdd7a6719132e39470061443a9de167
Justin Sherrell <Superjustin44@gmail.com>
2023-11-07 14:07:26 -0800

    Update practice_Stacks.java

--- Stack/practice_Stacks.java
+++ Stack/practice_Stacks.java
@@ -3,19 +3,23 @@ import java.util.*;
 public class practice_Stacks {
 
 
-    public static boolean isValid(String s) {
-        Stack<Character> stack = new Stack<>();
+    public static boolean isValidHashMap(String s) {
+        Stack<Character> brackets = new Stack<>();
+        Map<Character, Character> bracketMap = new HashMap<>();
+
+        bracketMap.put(')', '(');
+        bracketMap.put('}', '{');
+        bracketMap.put(']', '[');
 
         for (char c : s.toCharArray()) {
-            if (stack.isEmpty() && (c == ')' || c == '}' || c == ']')) return false;
-            else {
-                if (c == ')' && stack.peek() == '(') stack.pop();
-                else if (c == '}' && stack.peek() == '{') stack.pop();
-                else if (c == ']' && stack.peek() == '[') stack.pop();
-                else stack.add(c);
-            }
+            if (bracketMap.containsKey(c)) {
+                if (!brackets.isEmpty() && bracketMap.get(c) == brackets.peek()) {
+                    brackets.pop();
+                } else return false;
+            } else brackets.push(c);
+
         }
-        return stack.isEmpty();
+        return brackets.isEmpty();
     }
 
 
