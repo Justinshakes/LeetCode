760a8a49ad3a36b2c26260bde43795f48dde18cc
Justin Sherrell <Superjustin44@gmail.com>
2023-11-12 00:33:28 -0800

    invertTree

--- BinaryTree/PracticeBiTrees.java
+++ BinaryTree/PracticeBiTrees.java
@@ -23,33 +23,18 @@ public class PracticeBiTrees {
         }
     }
 
-    public static boolean isBalanced(TreeNode root) {
-        // If the tree is empty, it is balanced
+    public static TreeNode invertTree(TreeNode root) {
         if (root == null)
-            return true;
+            return null;
 
-        // Check the balance of the left and right subtrees
-        int leftHeight = getHeight(root.left);
-        int rightHeight = getHeight(root.right);
+        TreeNode left = invertTree(root.left);
+        TreeNode right = invertTree(root.right);
 
-        // If the difference in heights is more than 1, the tree is not balanced
-        if (Math.abs(leftHeight - rightHeight) > 1)
-            return false;
+        // swap nodes
+        root.right = left;
+        root.left = right;
 
-        // Recursively check the balance of the left and right subtrees
-        return isBalanced(root.left) && isBalanced(root.right);
-    }
-
-    private static int getHeight(TreeNode node) {
-        if (node == null)
-            return 0;
-
-        // Return the maximum height plus 1 for the current node
-        int leftHeight = getHeight(node.left);
-        int rightHeight = getHeight(node.right);
-
-        // Return the maximum height plus 1 for the current node
-        return Math.max(leftHeight, rightHeight) + 1;
+        return root;
     }
 
 
