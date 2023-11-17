56923596ae4dac14075a9d69d674f540054eab8d
Justinshakes <Superjustin44@gmail.com>
2023-11-16 16:45:13 -0800

    isBalanced LEETCODE!

--- BinaryTree/BalancedBiTree_110/balTree_Practice.java
+++ BinaryTree/BalancedBiTree_110/balTree_Practice.java
@@ -25,26 +25,25 @@ public class balTree_Practice {
     }
 
     public static boolean isBalanced(TreeNode root) {
-        if (root == null)
-            return true;
-
-        int leftHeight = getHeight(root.left);
-        int rightHeight = getHeight(root.right);
-
-        if (Math.abs(leftHeight - rightHeight) > 1)
-            return false;
-
-        return isBalanced(root.left) && isBalanced(root.right);
+        return heightAndBalanced(root) != -1;
     }
 
-    public static int getHeight(TreeNode node) {
+    private static int heightAndBalanced(TreeNode node) {
         if (node == null)
             return 0;
 
-        int leftHeight = 1 + getHeight(node.left);
-        int rightHeight = 1 + getHeight(node.right);
+        int leftHeight = heightAndBalanced(node.left);
+        if (leftHeight == -1)
+            return -1;
 
-        return 1 + Math.max(leftHeight, rightHeight);
+        int rightHeight = heightAndBalanced(node.right);
+        if (rightHeight == -1)
+            return -1;
+
+        if (Math.abs(leftHeight - rightHeight) > 1)
+            return -1;
+
+        return Math.max(leftHeight, rightHeight) + 1;
     }
 
 
