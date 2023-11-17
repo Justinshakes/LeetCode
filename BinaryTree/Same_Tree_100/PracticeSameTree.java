3a46963cf308edce9fc8fbba7bde0fe3f4092130
Justinshakes <Superjustin44@gmail.com>
2023-11-17 00:27:18 -0800

    Update PracticeSameTree.java

--- BinaryTree/Same_Tree_100/PracticeSameTree.java
+++ BinaryTree/Same_Tree_100/PracticeSameTree.java
@@ -1,4 +1,47 @@
 package Same_Tree_100;
 
 public class PracticeSameTree {
+
+    public static class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode() {
+        }
+
+        TreeNode(int val) {
+            this.val = val;
+        }
+
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+
+        public String toString() {
+            return "Node: " + val;
+        }
+    }
+
+    public static boolean isSameTree(TreeNode p, TreeNode q) {
+        return dfs(q, p);
+    }
+
+    public static boolean dfs(TreeNode p, TreeNode q) {
+        if ((p == null) && (q == null))
+            return true;
+
+        if ((p == null) || (q == null))
+            return false;
+
+        if (p.val != q.val)
+            return false;
+
+        boolean left = dfs(p.left, q.left);
+        boolean right = dfs(p.right, q.right);
+
+        return left && right;
+    }
 }
