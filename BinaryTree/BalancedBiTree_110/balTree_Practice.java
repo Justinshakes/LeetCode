b08575072fca341f6c54829038bfa1985889827a
Justinshakes <Superjustin44@gmail.com>
2023-11-16 16:23:22 -0800

    Balanced o(n^2) solution

--- BinaryTree/BalancedBiTree_110/balTree_Practice.java
+++ BinaryTree/BalancedBiTree_110/balTree_Practice.java
@@ -25,23 +25,24 @@ public class balTree_Practice {
     }
 
     public static boolean isBalanced(TreeNode root) {
-        return heightAndBalanced(root) != -1;
+        if (root == null)
+            return true;
+
+        int leftHeight = getHeight(root.left);
+        int rightHeight = getHeight(root.right);
+
+        if (Math.abs(leftHeight - rightHeight) > 1)
+            return false;
+
+        return isBalanced(root.left) && isBalanced(root.right);
     }
 
-    private static int heightAndBalanced(TreeNode node) {
+    public static int getHeight(TreeNode node) {
         if (node == null)
             return 0;
 
-        int leftHeight = heightAndBalanced(node.left);
-        if (leftHeight == -1)
-            return -1;
-
-        int rightHeight = heightAndBalanced(node.right);
-        if (rightHeight == -1)
-            return -1;
-
-        if (Math.abs(leftHeight - rightHeight) > 1)
-            return -1;
+        int leftHeight = 1 + getHeight(node.left);
+        int rightHeight = 1 + getHeight(node.right);
 
         return 1 + Math.max(leftHeight, rightHeight);
     }
