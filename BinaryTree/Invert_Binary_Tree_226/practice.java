bba146037445637e2f355e1bb8507779caedb34e
Justin Sherrell <Superjustin44@gmail.com>
2023-12-02 12:44:33 -0800

    Update practice.java

--- BinaryTree/Invert_Binary_Tree_226/practice.java
+++ BinaryTree/Invert_Binary_Tree_226/practice.java
@@ -25,6 +25,15 @@ public class practice {
     }
 
     public static TreeNode invertTree(TreeNode root) {
+        if (root == null)
+            return null;
 
+        TreeNode left = invertTree(root.left);
+        TreeNode right = invertTree(root.right);
+
+        root.right = left;
+        root.left = right;
+
+        return root;
     }
 }
