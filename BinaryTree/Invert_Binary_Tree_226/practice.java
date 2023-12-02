f05481e68d1354cffd6497f97bea58b071fa6e9f
Justin Sherrell <Superjustin44@gmail.com>
2023-12-02 08:06:42 -0800

    Create practice.java

---
+++ BinaryTree/Invert_Binary_Tree_226/practice.java
@@ -0,0 +1,30 @@
+package Invert_Binary_Tree_226;
+
+public class practice {
+    public static class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode() {
+        }
+
+        TreeNode(int val) {
+            this.val = val;
+        }
+
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+
+        public String toString() {
+            return "NODE: " + val;
+        }
+    }
+
+    public static TreeNode invertTree(TreeNode root) {
+
+    }
+}
